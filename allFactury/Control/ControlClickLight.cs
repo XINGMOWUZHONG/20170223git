11110b9c329a57cbd62e7eb3f7fd5376704b1342
gitwge <632947072@qq.com>
2017-05-03 23:20:40 +0800

    20170503 wge

---
+++ allFactury/Control/ControlClickLight.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace WZYB.Control
+{
+   
+    public class ControlClickLight
+    {
+        public bool IsStart = false;
+        private int ThreadTimeClick = int.Parse(System.Configuration.ConfigurationManager.AppSettings["CLICK_sleeptime"].ToString());
+        private int ThreadTimeLight = int.Parse(System.Configuration.ConfigurationManager.AppSettings["LIGHT_sleeptime"].ToString());
+        public int handle = 1;
+        public string LinkStr = "";
+        public int TypeStr = 0;
+        GetIdex gi;
+        public int MachineCount = 5;
+        private int Click_Type_index;
+        private int Click_Num_index;
+        private int Click_Reset_index;
+        public string MachineCountStr = "ATTRIBUTE01_light**_LIGHT_STATE1";
+
+        #region click
+        public ControlClickLight()
+        {
+            IsStart = true;
+            gi = new GetIdex();
+            Click_Type_index = GetIdex.getDicInputIndex("ATTRIBUTE01_CLICK_TYPE");
+            Click_Num_index = GetIdex.getDicInputIndex("ATTRIBUTE01_CLICK_NUM");
+            Click_Reset_index = GetIdex.getDicOutputIndex(" ATTRIBUTE01_CLICK_RETURN");
+        }
+
+        public void ClickThreadFunc(object obj)
+        {
+            try
+            { 
+                while (true)
+                {
+                    if (IsStart)
+                    {
+                        uint result_type ;
+                        uint result_Num  ;
+                        ComTCPLib.GetInputAsUINT(handle, Click_Type_index, out result_type);
+                        Thread.Sleep(200);
+                        if (result_type > 0)
+                        {
+                            ComTCPLib.GetInputAsUINT(handle, Click_Num_index, out result_Num);
+                            LinkStr = ControlInterfaceMethod.getLinkByTypeAndNum((int)result_type, result_Num.ToString ());
+                            TypeStr = (int)result_type;
+                            IsStart = false;
+                        }
+                    }
+                    Thread.Sleep(ThreadTimeClick);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ResetClick( )
+        {
+            try
+            { 
+                if (!IsStart)
+                {
+                    ComTCPLib.SetOutputAsUINT(handle, Click_Reset_index, 1);
+                    Thread.Sleep(400);
+                    ComTCPLib.SetOutputAsUINT(handle, Click_Reset_index, 0);
+                    IsStart = true;
+                }
+                Thread.Sleep(ThreadTimeClick); 
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+
+        #region  light 
+        public void LightThreadFunc(object obj)
+        {
+            try
+            {
+                int num = Convert.ToInt32(obj);
+                while (true)
+                {
+                    if (IsStart)
+                    {
+                        setLightState(num.ToString());
+                    }
+                    Thread.Sleep(ThreadTimeLight);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void setLightState(string num)
+        {
+            //return;
+            string indexstr = MachineCountStr.Replace("**", num);
+            int index = GetIdex.getDicOutputIndex(indexstr);
+            int state = ControlInterfaceMethod.getMachineLightState(num);
+            //gi.updateValue(indexstr,state.ToString (),1,handle);
+            ComTCPLib.SetOutputAsUINT(1, index, (uint)state);
+        }
+
+        #endregion
+
+    }
+}
--- allFactury/PccNew/ControlThreadClickandLight.cs
+++ allFactury/PccNew/ControlThreadClickandLight.cs
@@ -11,70 +11,22 @@ namespace PccNew
 
     public class ControlThreadClickandLight
     {
-        private ControlClickAndLightThread clickControl = new ControlClickAndLightThread();
-        private Thread[] clickThread;
+        private ControlClickLight clickControl = new ControlClickLight();
+        private Thread clickThread;
 
 
-        //type 1agv 2ddj  3pcc 4csc 5ocs 6 screen
+        //type 1agv 3ddj  2pcc 4ssj 5platform 7ocs 6 screen machine
         private void StartClickThread()
         {
-            //if (clickThread == null)
-                clickThread = new Thread[clickControl.AllCount];
-            int m = 0;
-            for (int i = 0; i < clickControl.AgvCount; i++)
-            {
-                string[] array = { (i+1).ToString (), "1" };
-                clickThread[m] = new Thread(new ParameterizedThreadStart(clickControl.ClickThreadFunc));
-                clickThread[m].Start(array);
-                m++;
-            }
-
-            for (int i = 0; i < clickControl.DdjCount; i++)
-            {
-                string[] array = { (i + 1).ToString(), "2" };
-                clickThread[m] = new Thread(new ParameterizedThreadStart(clickControl.ClickThreadFunc));
-                clickThread[m].Start(array);
-                m++;
-            }
-
-            for (int i = 0; i < clickControl.PccCount; i++)
-            {
-                string[] array = { (i + 1).ToString(), "3" };
-                clickThread[m] = new Thread(new ParameterizedThreadStart(clickControl.ClickThreadFunc));
-                clickThread[m].Start(array);
-                m++;
-            }
-
-            for (int i = 0; i < clickControl.CscCount; i++)
-            {
-                string[] array = { (i + 1).ToString(), "4" };
-                clickThread[m] = new Thread(new ParameterizedThreadStart(clickControl.ClickThreadFunc));
-                clickThread[m].Start(array);
-                m++;
-            }
-
-            for (int i = 0; i < clickControl.OcsCount; i++)
-            {
-                string[] array = { (i + 1).ToString(), "5" };
-                clickThread[m] = new Thread(new ParameterizedThreadStart(clickControl.ClickThreadFunc));
-                clickThread[m].Start(array);
-                m++;
-            }
-
-            for (int i = 0; i < clickControl.MachineCount; i++)
-            {
-                string[] array = { (i + 1).ToString(), "6" };
-                clickThread[m] = new Thread(new ParameterizedThreadStart(clickControl.ClickThreadFunc));
-                clickThread[m].Start(array);
-                m++;
-            }
+            clickThread = new Thread(new ParameterizedThreadStart(clickControl.ClickThreadFunc));
+            clickThread.Start();
+             
         }
 
         private Thread[] LightThread;
         private void StartLightThread()
-        {
-            //if (LightThread == null)
-                LightThread = new Thread[clickControl.MachineCount];
+        { 
+             LightThread = new Thread[clickControl.MachineCount];
             for (int i = 0; i < clickControl.MachineCount; i++)
             {
                 LightThread[i] = new Thread(new ParameterizedThreadStart(clickControl.LightThreadFunc));
@@ -90,15 +42,14 @@ namespace PccNew
 
         public void threadStopAll()
         {
-            if (clickThread!=null)
-            for (int i = 0; i < clickThread.Length; i++)
-            {
-                clickThread[i].Abort();
-            }
+            if (clickThread!=null) 
+                clickThread.Abort();
             if (LightThread != null)
-            for (int i = 0; i < LightThread.Length; i++)
             {
-                LightThread[i].Abort();
+                for (int i = 0; i < LightThread.Length; i++)
+                {
+                    LightThread[i].Abort();
+                }
             }
         }
 
@@ -114,7 +65,7 @@ namespace PccNew
 
         public string[] getLinkAndType()
         {
-            if (clickControl !=null && clickControl.IsStart == false &&  clickControl.TypeStr != 0 && clickControl.LinkStr .Length >0 )
+            if (clickControl !=null && clickControl.IsStart == false &&  clickControl.TypeStr != 0 && clickControl.LinkStr.Length >0 )
             {
                 string[] arrayA = {clickControl.TypeStr.ToString (),clickControl.LinkStr };
                 setLinkAndType();
@@ -129,6 +80,7 @@ namespace PccNew
             {
                 clickControl.TypeStr = 0;
                 clickControl.LinkStr = "";
+                clickControl.ResetClick();
             }
         }
     }
